be05075bbfbd9efd86b62d78ced3abc5a98a9c08
pietroarsi <arsipietro@gmail.com>
2021-12-02 16:48:57 +0100

    actions

--- make_rm.py
+++ make_rm.py
@@ -2,14 +2,14 @@ import os
 from jinja2 import Template
 
 years=[2019, 2020, 2021]
-dirnames = [f"day{str(day).rjust(2, '0')}" for day in range(1, 26)]
+dirnames = [f"day{day:02}" for day in range(1, 26)]
 
 file_check = {}
 for year in years:
     file_check[year] = {}
     for day in range(1, 26):
         try:
-            file_check[year][day] = (1 if len(os.listdir(path = f"{year}/day{str(day).rjust(2, '0')}")) > 0 else 0)
+            file_check[year][day] = (1 if len(os.listdir(path = f"{year}/day{day:02}")) > 0 else 0)
         except:
             file_check[year][day] = 0
     
