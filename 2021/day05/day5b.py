2003db6c1237beb093b6df97b73e96e5134ded2d
pietroarsi <arsipietro@gmail.com>
2021-12-05 17:33:14 +0100

    dict -> defaultdict

--- 2021/day05/day5b.py
+++ 2021/day05/day5b.py
@@ -1,45 +1,18 @@
-def add_tup(t, ps, eps):
-    global count
-    # if(t in ps and t in eps):
-    #     eps.append(t)
-    # elif(t in ps and t not in eps):
-    #     count += 1
-    #     eps.append(t)
-    # elif (not t in ps and not t in eps):
-    #     points.add(t)
-    if(t[0] in ps.keys()):
-        if(t[1] in ps[t[0]]):
-            if (t[0] in eps.keys() and t[1] in eps[t[0]]):
-                eps[t[0]].append(t[1])
-            elif(t[0] in eps.keys() and t[1] not in eps[t[0]]):
-                count += 1
-                eps[t[0]].append(t[1])
-            elif(t[0] not in eps.keys()):
-                count += 1
-                eps[t[0]] = list()
-                eps[t[0]].append(t[1])
-        else:
-            ps[t[0]].append(t[1])
-    else:
-        ps[t[0]] = list()
-        ps[t[0]].append(t[1])
+from collections import defaultdict
 
 with open('i5.txt', 'r') as f:
     data = [[[int(z) for z in y.split(",")] for y in x.strip().split(" -> ")] for x in f.readlines()]
 
-points = dict()
-extra_points = dict()
-count = 0
+points = defaultdict(int)
 for x in data:
     if (x[0][0] == x[1][0]):
         # x1 = x2
         coord = x[0][0]
         min_y = min(x[0][1], x[1][1])
         max_y = max(x[0][1], x[1][1])
-
         for p in range(min_y, max_y + 1):
             tup = (int(coord), int(p))
-            add_tup(tup, points, extra_points)
+            points[tup] += 1
     elif (x[0][1] == x[1][1]):
         # y1 = y2
         coord = x[0][1]
@@ -47,7 +20,7 @@ for x in data:
         max_x = max(x[0][0], x[1][0])
         for p in range(min_x, max_x + 1):
             tup = (int(p), int(coord))
-            add_tup(tup, points, extra_points)
+            points[tup] += 1
     else:
         # print(x)
         x_diff = x[1][0] - x[0][0]
@@ -71,37 +44,32 @@ for x in data:
                 j = x[0][1] + diff
                 tup = (int(i), int(j))
                 # print(f"-> {tup}")
-                add_tup(tup, points, extra_points)
+                points[tup] += 1
         elif(quadrante == 2):
             for diff in range(abs(x[1][0] - x[0][0]) + 1):
                 i = x[0][0] - diff
                 j = x[0][1] + diff
                 tup = (int(i), int(j))
                 # print(f"-> {tup}")
-                add_tup(tup, points, extra_points)
+                points[tup] += 1
         elif(quadrante == 3):
             for diff in range(abs(x[1][0] - x[0][0]) + 1):
                 i = x[0][0] - diff
                 j = x[0][1] - diff
                 tup = (int(i), int(j))
                 # print(f"-> {tup}")
-                add_tup(tup, points, extra_points)
+                points[tup] += 1
         elif(quadrante == 4):
             for diff in range(abs(x[1][0] - x[0][0]) + 1):
                 i = x[0][0] + diff
                 j = x[0][1] - diff
                 tup = (int(i), int(j))
                 # print(f"-> {tup}")
-                add_tup(tup, points, extra_points)
+                points[tup] += 1
+
+count = 0
+for x in points.keys():
+    if(points[x] > 1):
+        count += 1
 
 print(f"Result: {count}")
-
-# map = [[0 for x in range(10)] for y in range(10)]
-
-# for x in points:
-#     map[x[1]][x[0]] += 1
-# for x in extra_points:
-#     map[x[1]][x[0]] += 1
-
-# for x in map:
-#     print(" ".join([str(y) for y in x]))
