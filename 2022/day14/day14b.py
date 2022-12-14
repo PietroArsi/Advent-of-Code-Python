8753f3bbf8541608f7af42bdae29be0fbb4ccbad
pietroarsi <arsipietro@gmail.com>
2022-12-14 16:02:05 +0100

    day 14 finito

--- 2022/day14/day14b.py
+++ 2022/day14/day14b.py
@@ -1,5 +1,21 @@
 import re
 
+def printmap(rocks, s):
+    f = open("2022/day14/out.txt", "a", encoding="utf-8")
+    for y in range(0, 12):
+        for x in range(470, 520):
+            if y == 0 and x == 500:
+                f.write("+")
+            elif x in rocks.keys() and y in rocks[x]:
+                f.write("#")
+            elif x in s.keys() and y in s[x]:
+                f.write("o")
+            else:
+                f.write(".")
+        f.write("\n")
+    f.write("\n")
+    f.close()
+
 with open("2022/day14/input.txt", encoding="utf-8") as f:
     lines = [x.strip() for x in f.readlines()]
 
@@ -8,9 +24,9 @@ for l in lines:
     m = re.findall(r"(\d+),(\d+)", l)
     coords.append([(int(x[0]), int(x[1])) for x in m])
 
-# print(coords)
 lowest = None
-map = set()
+rockmap = dict()
+
 for c in coords:
     for x in range(0, len(c)-1):
         if lowest == None or c[x][1] > lowest:
@@ -21,42 +37,63 @@ for c in coords:
             b = max(c[x][1], c[x+1][1])
             a = min(c[x][1], c[x+1][1])
             for y in range(a, b+1):
-                map.add((c[x][0], y))
+                if c[x][0] not in set(rockmap.keys()):
+                    rockmap[c[x][0]] = set()
+                rockmap[c[x][0]].add(y)
         elif c[x][1] == c[x+1][1]:
             b = max(c[x][0], c[x+1][0])
             a = min(c[x][0], c[x+1][0])
             for y in range(a, b+1):
-                map.add((y, c[x][1]))
+                if y not in set(rockmap.keys()):
+                    rockmap[y] = set()
+                rockmap[y].add(c[x][1])
         else:
             print("ERROR")
 
-# print(lowest)
-# print(map)
+# f = open("2022/day14/out.txt", "w")
+# f.close()
 
 lowest += 2
 
-sands = set()
+sands = dict()
 count = 0
+
+total_map = {k: rockmap.get(k, set()).union(sands.get(k, set())) for k in set(rockmap.keys()).union(set(sands.keys()))}
+
 while True:
     sand = (500,0)
+    # printmap(rockmap, sands)
 
     exit = False
     print(count)
     while True:
-        # print(sand)
+        # total_map = {k: rockmap.get(k, set()).union(sands.get(k, set())) for k in set(rockmap.keys()).union(set(sands.keys()))}
+
         if sand[1] == lowest-1:
             count += 1
-            sands.add(sand)
+            # if sand[0] not in set(sands.keys()):
+            #     sands[sand[0]] = set()
+            if sand[0] not in set(total_map.keys()):
+                total_map[sand[0]] = set()
+            total_map[sand[0]].add(sand[1])
+            # sands[sand[0]].add(sand[1])
             break
-        elif (sand[0], sand[1]+1) not in map.union(sands):
+        elif sand[0] not in set(total_map.keys()) or sand[1]+1 not in total_map[sand[0]]:
             sand = (sand[0], sand[1]+1)
-        elif (sand[0]-1, sand[1]+1) not in map.union(sands):
+        elif sand[0]-1 not in set(total_map.keys()) or sand[1]+1 not in total_map[sand[0]-1]:
             sand = (sand[0]-1, sand[1]+1)
-        elif (sand[0]+1, sand[1]+1) not in map.union(sands):
+        elif sand[0]+1 not in set(total_map.keys()) or sand[1]+1 not in total_map[sand[0]+1]:
             sand = (sand[0]+1, sand[1]+1)
         else:
             count += 1
-            sands.add(sand)
+
+            # if sand[0] not in set(sands.keys()):
+            #     sands[sand[0]] = set()
+            if sand[0] not in set(total_map.keys()):
+                total_map[sand[0]] = set()
+            total_map[sand[0]].add(sand[1])
+            # sands[sand[0]].add(sand[1])
+
             if sand == (500,0):
                 exit = True
             break
